5868999a8ef9f025fdd019cca25d503fd598a813
yzm <13884935@qq.com>
2024-03-24 23:59:55 +0900

    1

---
+++ next.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+    images: {
+      domains: ['aceternity.com'], // 在这个数组中添加其他你需要加载图片的域名
+    },
+  }
+  
--- src/app/page—单个.tsx
+++ src/app/page—单个.tsx
@@ -1,54 +1,10 @@
 "use client";
-import { useScroll, useTransform } from "framer-motion";
 import React from "react";
-import { GoogleGeminiEffect } from "@/components/ui/google-gemini-effect";
 import { HeroParallax } from "@/components/ui/hero-parallax";
-import { TextGenerateEffect } from "@/components/ui/text-generate-effect";
 
-
-export default function GoogleGeminiEffectDemo() {
-  const ref = React.useRef(null);
-  const { scrollYProgress } = useScroll({
-    target: ref,
-    offset: ["start start", "end start"],
-  });
-
-  const pathLengthFirst = useTransform(scrollYProgress, [0, 0.8], [0.2, 1.2]);
-  const pathLengthSecond = useTransform(scrollYProgress, [0, 0.8], [0.15, 1.2]);
-  const pathLengthThird = useTransform(scrollYProgress, [0, 0.8], [0.1, 1.2]);
-  const pathLengthFourth = useTransform(scrollYProgress, [0, 0.8], [0.05, 1.2]);
-  const pathLengthFifth = useTransform(scrollYProgress, [0, 0.8], [0, 1.2]);
-
-  return (
-    <>
-    <div
-      className="h-[400vh] bg-black w-full dark:border dark:border-white/[0.1] rounded-md relative pt-40 overflow-clip"
-      ref={ref}
-    >
-      <GoogleGeminiEffect
-        pathLengths={[
-          pathLengthFirst,
-          pathLengthSecond,
-          pathLengthThird,
-          pathLengthFourth,
-          pathLengthFifth,
-        ]}
-      />
-    </div>
-
-    
-    <div>
-    <HeroParallax products={products} />
-    </div> 
-
-
-    
-
-    </>
-  );
+export default function HeroParallaxDemo() {
+  return <HeroParallax products={products} />;
 }
-
-
 export const products = [
   {
     title: "Moonbeam",
@@ -68,7 +24,7 @@ export const products = [
     thumbnail:
       "https://aceternity.com/images/products/thumbnails/new/rogue.png",
   },
- 
+
   {
     title: "Editorially",
     link: "https://editorially.org",
@@ -87,7 +43,7 @@ export const products = [
     thumbnail:
       "https://aceternity.com/images/products/thumbnails/new/pixelperfect.png",
   },
- 
+
   {
     title: "Algochurn",
     link: "https://algochurn.com",
@@ -118,7 +74,7 @@ export const products = [
     thumbnail:
       "https://aceternity.com/images/products/thumbnails/new/renderwork.png",
   },
- 
+
   {
     title: "Creme Digital",
     link: "https://cremedigital.com",
