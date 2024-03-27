1fed8fafb472a0bf6a4d0c69b3dcc0fd1f8395d7
yzm <13884935@qq.com>
2024-03-27 14:01:38 +0900

    1

--- src/components/ui/google-gemini-effect.tsx
+++ src/components/ui/google-gemini-effect.tsx
@@ -22,7 +22,7 @@ export const GoogleGeminiEffect = ({
   return (
     <div className={cn("sticky top-80", className)}>
       <p className="text-lg md:text-5xl font-normal pb-4 text-center bg-clip-text text-transparent bg-gradient-to-b from-neutral-100 to-neutral-300">
-        {title || `Hoopelife Was Born Into Healthcare`}
+        {title || `RTFchain Was Born Into Healthcare`}
       </p>
       <p className="text-xs md:text-xl font-normal text-center text-neutral-400 mt-4 max-w-lg mx-auto">
         {description ||
